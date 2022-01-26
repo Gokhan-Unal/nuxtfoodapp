207377604abd1563d73f385f5dc9be563632d159
Gökhan Ünal <unal_gokhan@outlook.com>
2022-01-26 19:01:24 +0300

    almost initial setup

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+html {
+  font-family: "Mullish", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-weight: 300;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+  margin: 0;
+}
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Poppins", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-weight: 600;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="container">
+    <h1>Hi From Home</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
