868334524a1b92f6ee9c1f477f19c172a3b80b9e
Gökhan Ünal <unal_gokhan@outlook.com>
2022-01-27 02:02:47 +0300

    Restaurant Grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <main class="container">
     <app-header />
+    <app-restaurant-info />
   </main>
 </template>
 
 <script>
 import AppHeader from "../components/AppHeader.vue";
+import AppRestaurantInfo from "../components/AppRestaurantInfo.vue";
 export default {
-  components: { AppHeader },
+  components: { AppHeader, AppRestaurantInfo },
   name: "IndexPage",
 };
 </script>
